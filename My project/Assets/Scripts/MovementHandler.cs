5632b0734bcc421947d7140f12f218cddd0b85fd
Almaz <41963210+craybob@users.noreply.github.com>
2024-09-02 00:01:46 +0300

    GetCam directiorn

--- My project/Assets/Scripts/MovementHandler.cs
+++ My project/Assets/Scripts/MovementHandler.cs
@@ -28,8 +28,43 @@ public class MovementHandler
     private Vector3 GetMoveDirection(){
         float xDirertion = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(xDirertion * speed, 0f, zDirection * speed);
+        Vector3 moveDirection = GetCamRotation(zDirection, xDirertion);//new Vector3(xDirertion * speed, 0f, zDirection * speed);
 
         return moveDirection;
+    }
+
+    private Vector3 GetCamRotation(float directionZ, float directionX)
+    {
+        // --- Character rotation --- 
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        // Relate the front with the Z direction (depth) and right with X (lateral movement)
+        forward = forward * directionZ;
+        right = right * directionX;
+
+        if (directionX != 0 || directionZ != 0)
+        {
+            float angle = Mathf.Atan2(forward.x + right.x, forward.z + right.z) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            transformObj.rotation = Quaternion.Slerp(transformObj.rotation, rotation, 0.15f);
+        }
+
+        // --- End rotation ---
+        
+
+        //Vector3 verticalDirection = Vector3.up * Physics.gravity.y;
+        Vector3 horizontalDirection = forward + right;
+
+        Vector3 moviment =  horizontalDirection;
+
+        return moviment;
     }
 }
--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player: MonoBehaviour
 
     
     [SerializeField] public float _jumpForce = 1f;
+    [SerializeField] public float _speed = 5f;
 
     [Inject]
     private void Construct(MovementHandler movementHandler)
@@ -23,13 +24,24 @@ public class Player: MonoBehaviour
     {
         _movementHandler.rb = GetComponent<Rigidbody>();
         _movementHandler.transformObj = transform;
-        _movementHandler.speed = 5f;
+        _movementHandler.speed = _speed;
         _movementHandler.jumpForce = _jumpForce;
     }
     
     private void Update()
     {
         MoveMethods();
+
+        //if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        //{
+        //    Debug.Log(new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        //}
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    Vector3 tot =  new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //    tot.Normalize();
+        //    Debug.Log(tot);
+        //}
     }
 
     private void MoveMethods()
